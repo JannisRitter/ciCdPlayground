20a38f09d88c7c409d81371786b4600a9aa96ea4
JannisRitter <133093099+JannisRitter@users.noreply.github.com>
2026-07-24 14:16:04 +0200

    fdasg

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ new App({
     // What do you associate with the term 'CI/CD'?
     associations: ["pipelines, testing, automation"],
     // Which CI/CD tools do you use in your project?
-    tools: "Github Actions (I think)",
+    tools: "Jenkins (I think)",
     // What do you want to learn in this workshop?
     expectations: ["better understanding"],
   },
